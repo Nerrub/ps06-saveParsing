87c54c4136c9bd7c5193a2f2cee36a541e770150
Nerrub <Lyapin_dmitriy@hotmail.com>
2024-07-04 13:53:37 +0300

    2

--- main.py
+++ main.py
@@ -3,29 +3,14 @@ import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
-# class DivannewparsSpider(scrapy.Spider):
-#     name = "divannewpars"
-#     allowed_domains = ["https://divan.ru"]
-#     start_urls = ["https://www.divan.ru/category/svet"]
-#
-#     def parse(self, response):
-#         divans = response.css('div._Ud0k')
-#         for divan in divans:
-#             yield {
-#                 'name' : divan.css('div.wYUX2 span::text').get(),
-#                 'price' : divan.css('div.pY3d2 span::text').get(),
-#                 'url': divan.css('a').attrib['href']
-#             }
-
 driver = webdriver.Chrome()
 url = "https://www.divan.ru/category/svet"
 
 driver.get(url)
 
-time.sleep(3)
+time.sleep(10)
 
-svets = driver.find_elements(By.CLASS_NAME, 'WdR1o')
+svets = driver.find_elements(By.CLASS_NAME, '_Ud0k U4KZV')
 
 parsed_data = []
 
@@ -33,7 +18,7 @@ for svet in svets:
     try:
         name = svet.find_element(By.CSS_SELECTOR, 'span.ui-GPFV8 qUioe ProductName ActiveProduct').text
         price = svet.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU KIkOH').text
-        link = svet.find_element(By.CSS_SELECTOR, 'a.ui-GPFV8 qUioe ProductName ActiveProduct').get_attribute('href')
+        link = svet.find_element(By.CSS_SELECTOR, 'a.class=ui-GPFV8 qUioe ProductName ActiveProduct').get_attribute('href')
     except:
         print('Произошла ошибка при парсинге')
         continue
@@ -42,7 +27,7 @@ for svet in svets:
 
 driver.quit()
 
-with open("hh.csv", 'w', newline='', encoding='utf-8-sig') as file:
+with open("svet.csv", 'w', newline='', encoding='utf-8-sig') as file:
     writer = csv.writer(file)
     writer.writerow(['Название','Цена','Ссылка на товар'])
     writer.writerows(parsed_data)
